f113c33811784b6821761cd363e6431ae3de691d
Clinsen <clinsenmain@gmail.com>
2025-10-14 04:18:31 +0300

    Create TestData class

---
+++ src/test/java/com/demo/tests/data/TestData.java
@@ -0,0 +1,7 @@
+package com.demo.tests.data;
+
+public final class TestData {
+    private TestData() {}
+    public static final String SEARCH_EXISTING = "MacBook";
+    public static final String SEARCH_ABSENT   = "Non-existing data 12345 ^^$!";
+}
