55daf84944e2369991b8a1977bfc6323cdffd8f1
Clinsen <clinsenmain@gmail.com>
2025-11-20 13:30:15 +0200

    Refactor DriverFactory for stable CI Chrome setup

--- src/main/java/com/demo/framework/base/DriverFactory.java
+++ src/main/java/com/demo/framework/base/DriverFactory.java
@@ -2,7 +2,6 @@ package com.demo.framework.base;
 
 import com.demo.framework.config.Environment;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.time.Duration;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
@@ -11,35 +10,62 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.time.Duration;
+
 public class DriverFactory {
 
     public static WebDriver createDriver(String browserName) {
         WebDriver driver;
 
         switch (browserName.toLowerCase()) {
+
             case "firefox": {
                 WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions ff = new FirefoxOptions();
-                if (Environment.headless()) ff.addArguments("-headless");
+
+                if (Environment.headless()) {
+                    ff.addArguments("-headless");
+                }
+
                 ff.setPageLoadStrategy(PageLoadStrategy.NORMAL);
                 driver = new FirefoxDriver(ff);
                 break;
             }
+
             case "chrome":
             default: {
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions ch = new ChromeOptions();
-                if (Environment.headless()) ch.addArguments("--headless=new");
-                ch.addArguments("--disable-gpu", "--no-sandbox", "--window-size=1440,900");
+
+                // Headless mode for CI
+                if (Environment.headless()) {
+                    ch.addArguments("--headless=new");
+                }
+
+                // For Linux + CI environments
+                ch.addArguments(
+                        "--disable-gpu",
+                        "--no-sandbox",
+                        "--disable-dev-shm-usage",
+                        "--remote-allow-origins=*",
+                        "--window-size=1440,900"
+                );
+
                 ch.setPageLoadStrategy(PageLoadStrategy.NORMAL);
                 driver = new ChromeDriver(ch);
                 break;
             }
         }
 
+        // Global
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ZERO);
-        if (!Environment.headless()) driver.manage().window().setSize(new Dimension(1440, 900));
+
+        // For non-headless
+        if (!Environment.headless()) {
+            driver.manage().window().setSize(new Dimension(1440, 900));
+        }
+
         return driver;
     }
 }
