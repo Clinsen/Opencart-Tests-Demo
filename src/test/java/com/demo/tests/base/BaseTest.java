63cc291969e663d8ffd7ecd01bd5483ba5ed223e
Clinsen <clinsenmain@gmail.com>
2025-11-20 13:05:35 +0200

    Integrate Allure into the project

--- src/test/java/com/demo/tests/base/BaseTest.java
+++ src/test/java/com/demo/tests/base/BaseTest.java
@@ -1,21 +1,39 @@
 package com.demo.tests.base;
 
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import com.demo.framework.base.DriverFactory;
 import com.demo.framework.config.Environment;
 import com.demo.framework.driver.DriverManager;
 import com.demo.framework.pages.HomePage;
-import com.demo.framework.utils.ScreenshotUtils;
 import com.demo.framework.utils.Waits;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public abstract class BaseTest {
     protected HomePage home;
 
     protected WebDriver driver() {
         return DriverManager.getDriver();
+    }
+
+    @BeforeSuite(alwaysRun = true)
+    public void writeAllureEnvironment() throws Exception {
+        Path dir = Path.of("target", "allure-results");
+        Files.createDirectories(dir);
+        try (var w = new FileWriter(dir.resolve("environment.properties").toFile())) {
+            w.write("BaseURL=" + Environment.baseUrl() + "\n");
+            w.write("Browser=" + Environment.browser() + "\n");
+            w.write("Headless=" + Environment.headless() + "\n");
+            w.write("TimeoutSec=" + Environment.timeoutSec() + "\n");
+            w.write("OS=" + System.getProperty("os.name") + "\n");
+            w.write("Java=" + System.getProperty("java.version") + "\n");
+        }
     }
 
     @BeforeMethod(alwaysRun = true)
--- src/test/java/com/demo/tests/base/TestListener.java
+++ src/test/java/com/demo/tests/base/TestListener.java
@@ -1,60 +1,46 @@
 package com.demo.tests.base;
 
 import com.demo.framework.driver.DriverManager;
-import com.demo.framework.utils.ScreenshotUtils;
-import org.openqa.selenium.WebDriver;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static org.testng.ITestResult.FAILURE;
-import static org.testng.ITestResult.SUCCESS_PERCENTAGE_FAILURE;
+import java.time.Instant;
 
 public class TestListener implements ITestListener {
 
     @Override
-    public void onTestStart(ITestResult result) {
-        System.out.println("[START] " + result.getMethod().getMethodName() +
-                " | thread=" + Thread.currentThread().getId());
-    }
-
-    @Override
-    public void onTestSuccess(ITestResult result) {
-        System.out.println("[PASS]  " + result.getMethod().getMethodName());
-    }
-
-    @Override
     public void onTestFailure(ITestResult result) {
-        captureArtifacts(result);
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        System.out.println("[SKIP]  " + result.getMethod().getMethodName());
-    }
-
-    private void captureArtifacts(ITestResult result) {
-        WebDriver drv = DriverManager.getDriver();
-        if (drv == null) return;
+        var driver = DriverManager.getDriver();
+        if (driver instanceof TakesScreenshot ts) {
+            byte[] png = ts.getScreenshotAs(OutputType.BYTES);
+            Allure.getLifecycle().addAttachment(
+                    "Screenshot - " + Instant.now(),
+                    "image/png",
+                    "png",
+                    png
+            );
+        }
 
         try {
-            String shot = ScreenshotUtils.take(drv, result.getMethod().getMethodName());
-            System.out.println("[Screenshot] " + shot + " | URL: " + drv.getCurrentUrl());
-
-           /* String ts = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            Path htmlDir = Path.of("target", "pagesource");
-            Files.createDirectories(htmlDir);
-            Path htmlPath = htmlDir.resolve(result.getMethod().getMethodName() + "_" + ts + ".html");
-            Files.writeString(htmlPath, drv.getPageSource());
-            System.out.println("[HTML] " + htmlPath);*/
-
-        } catch (Exception e) {
-            System.out.println("Failed to capture artifacts: " + e.getMessage());
+            Allure.addAttachment("Current URL", "text/plain", driver.getCurrentUrl());
+        } catch (Exception ignored) {
         }
     }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        String name = result.getMethod().getMethodName();
+        Allure.addAttachment("Test start", "text/plain", "Starting: " + name);
+    }
+
+    @Override public void onTestSuccess(ITestResult result) { }
+    @Override public void onTestSkipped(ITestResult result) { }
+    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) { }
+    @Override public void onTestFailedWithTimeout(ITestResult result) { }
+    @Override public void onStart(ITestContext context) { }
+    @Override public void onFinish(ITestContext context) { }
 }
